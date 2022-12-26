3d642ee0c2c969267d5556b9f629e7040287da04
Herizki Wahyu Saputra <102897141+HerizkiWahyuSaputra@users.noreply.github.com>
2022-12-26 23:05:11 +0700

    Add files via upload

---
+++ Self Project Python Beginner/Mad Libs Python/madlibs/sample_madlibs/Aboutfuture.py
@@ -0,0 +1,29 @@
+def madlib():
+    adj1 = input("adjective1: ")
+    adj2 = input("adjective2: ")
+    adj3 = input("adjective3: ")
+    noun1 = input("noun1: ")
+    noun2 = input("noun2: ")
+    noun3 = input("noun3: ")
+    spell1 = input("spell: ")
+    hope = input("everything about hope people: ")
+
+    madlib= f"Many other people struggle to imagine their future self as a {adj1} of the person that they are today,\
+            and they tend to be far less responsible in their {noun1}. It’s almost as if they see their future self as a separate \
+            person that has little connection to their present identity – and, as a result, they are far {noun2} about\
+            the long-term consequences of their {adj2}.:n\
+            \"{spell1}!\"\n\
+            You could almost think about your future self as a relationship that needs to be nurtured and {adj3}. \
+            {noun3}, there are some simple strategies to strengthen your empathy and compassion for the person \
+            you will become – with some profound consequences for your health, happiness and {hope}." 
+
+print(madlib)
+
+# "Many other people struggle to imagine their future self as a continuation of the person that they are today,\
+# and they tend to be far less responsible in their behaviours. It’s almost as if they see their future self as a separate \
+# person that has little connection to their present identity – and, as a result, they are far less worried about\
+# the long-term consequences of their actions.
+
+# You could almost think about your future self as a relationship that needs to be nurtured and cultivated. \
+# Fortunately, there are some simple strategies to strengthen your empathy and compassion for the person \
+# you will become – with some profound consequences for your health, happiness and financial security."
---
+++ Self Project Python Beginner/Mad Libs Python/madlibs/sample_madlibs/Aboutindustry.py
@@ -0,0 +1,24 @@
+def madlib():
+    adj1 = input("adjective1: ")
+    adj2 = input("adjective2: ")
+    adj3 = input("adjective3: ")
+    noun1 = input("noun1: ")
+    noun2 = input("noun2: ")
+    noun3 = input("noun3: ")
+    spell1 = input("spell: ")
+
+    madlib= f"{adj1} is revolutionizing the way companies manufacture, improve and distribute their {adj2}. \
+            Manufacturers are integrating new technologies, including Internet of Things (IoT), cloud computing and analytics, \
+            and {adj3} into their production facilities and throughout their operations. :\n\
+            \"{spell1}!\"\n\
+            Industry 4.0 concepts and technologies can be applied across {noun1} of industrial companies, \
+            including discrete and process {noun2}, as well as oil and gas, mining and other {noun3}"
+
+print(madlib)
+
+# Industry 4.0 is revolutionizing the way companies manufacture, improve and distribute their products. \
+# Manufacturers are integrating new technologies, including Internet of Things (IoT), cloud computing and analytics, \
+# and AI and machine learning into their production facilities and throughout their operations.
+
+# Industry 4.0 concepts and technologies can be applied across all types of industrial companies, \
+# including discrete and process manufacturing, as well as oil and gas, mining and other industrial segments. 
