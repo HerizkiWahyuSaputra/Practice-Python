7b5179bc58bda34ff31c6aa581fef1719a694337
Herizki Wahyu Saputra <102897141+HerizkiWahyuSaputra@users.noreply.github.com>
2022-12-27 22:30:35 +0700

    Delete Self Project Python Beginner/Mad Libs Python/madlibs directory

--- Self Project Python Beginner/Mad Libs Python/madlibs/random_madlibs.py
+++
@@ -1,6 +0,0 @@
-from sample_madlibs import Aboutfuture, Aboutindustry, AboutTech
-import random 
-
-if __name__ == "__main__":
-    n = random.choice ([Aboutfuture, Aboutindustry, AboutTech])
-    n.madlib()
